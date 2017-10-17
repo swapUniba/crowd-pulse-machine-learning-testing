5a3dcfaed499175b38feade0532c648d5d10523d
vitoturing <cuccovito@gmail.com>
2017-10-17 12:40:02 +0200

    Initial Commit

---
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/testing/MachineLearningTestingConfig.java
@@ -0,0 +1,12 @@
+package com.github.swapUniba.pulse.crowd.machinelearning.testing;
+
+import com.github.frapontillo.pulse.spi.IPluginConfig;
+import com.github.frapontillo.pulse.spi.PluginConfigHelper;
+import com.google.gson.JsonElement;
+
+public class MachineLearningTestingConfig implements IPluginConfig<MachineLearningTestingConfig> {
+    @Override
+    public MachineLearningTestingConfig buildFromJsonElement(JsonElement jsonElement) {
+        return PluginConfigHelper.buildFromJson(jsonElement, MachineLearningTestingConfig.class);
+    }
+}
---
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/testing/MachineLearningTestingPlugin.java
@@ -0,0 +1,47 @@
+package com.github.swapUniba.pulse.crowd.machinelearning.testing;
+
+import com.github.frapontillo.pulse.crowd.data.entity.Message;
+import com.github.frapontillo.pulse.spi.IPlugin;
+import com.github.frapontillo.pulse.util.PulseLogger;
+import org.apache.logging.log4j.Logger;
+import rx.Observable;
+import rx.Subscriber;
+import rx.observers.SafeSubscriber;
+
+public class MachineLearningTestingPlugin extends IPlugin<Message,Message,MachineLearningTestingConfig> {
+
+    private static final String PLUGIN_NAME = "machine-learning-testing";
+    public static final Logger logger = PulseLogger.getLogger(MachineLearningTestingPlugin.class);
+
+    @Override
+    public String getName() {
+        return PLUGIN_NAME;
+    }
+
+    @Override
+    public MachineLearningTestingConfig getNewParameter() {
+        return new MachineLearningTestingConfig();
+    }
+
+    @Override
+    protected Observable.Operator<Message, Message> getOperator(MachineLearningTestingConfig machineLearningTestingConfig) {
+        return subscriber -> new SafeSubscriber<>(new Subscriber<Message>() {
+
+            @Override
+            public void onCompleted() {
+                subscriber.onCompleted();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                logger.error("ERRORE:" + e.toString());
+                subscriber.onError(e);
+            }
+
+            @Override
+            public void onNext(Message message) {
+                subscriber.onNext(message);
+            }
+        });
+    }
+}
