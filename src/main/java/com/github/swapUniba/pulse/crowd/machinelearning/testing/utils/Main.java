cdcd25be66e181884bf9ea0d4bb716c14055b2af
vitoturing <cuccovito@gmail.com>
2017-10-26 19:31:30 +0200

    Extended model testing on single or complete testing set. Added option handling

--- src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/testing/utils/Main.java
+++ src/main/java/com/github/swapUniba/pulse/crowd/machinelearning/testing/utils/Main.java
@@ -13,8 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //TestSingleInstance();
-        TestTestingSet();
+        TestSingleInstance();
+        //TestTestingSet();
 
     }
 
